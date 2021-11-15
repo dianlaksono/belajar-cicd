6341bbbf2aa318413d97035c78a4e0442893d983
dianlaksono <dianlaksono20@gmail.com>
2021-11-15 11:56:03 +0700

    typo PORT

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = process.env.POER || 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.status(200).json({message : 'BTPNS SHIFTED 3 '})
